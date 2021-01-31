4183b1fb155a2cae78b5c12c5089c87ff24f20ff
Jordan <jordancaley10@gmail.com>
2021-01-31 13:34:03 +0100

    Write player program

--- lib/application.rb
+++ lib/application.rb
@@ -3,6 +3,12 @@ class Application
     # TO DO : méthode qui initialise le jeu puis contient des boucles while pour faire tourner le jeu tant que la partie n'est pas terminée.
     # 
 
+    #Initialize players
+    puts "Player 1, you'll be X. What is your name?"
+    player1 = Player.new("#{gets.chomp}", "X")
+    puts "Player 2, you'll be O. What is your name?"
+    player2 = Player.new("#{gets.chomp}", "O")
+
   end
 
 end
